21d8188a0aeea47d2705fd26774c001481559806
saurabhzalke <saurabhc645@gmail.com>
2020-11-25 21:48:55 +0530

    First commit of this project that includes all files!

---
+++ Plotter.py
@@ -0,0 +1,19 @@
+from Motion_detector import df
+from bokeh.plotting import figure, show, output_file
+from bokeh.models import HoverTool, ColumnDataSource
+
+df["Start_string"]=df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_string"]=df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+
+cds=ColumnDataSource(df)
+
+p=figure(x_axis_type='datetime',height=100, width=500, sizing_mode = "scale_width", title="Motion Graph")
+
+hover=HoverTool(tooltips=[("Start","@Start_string"),("End","@End_string")])
+p.add_tools(hover)
+
+q=p.quad(left="Start",right="End",bottom=0,top=1,color="green",source=cds)
+
+output_file("Time_Log.html")
+show(p)
